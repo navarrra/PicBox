f83a098223afa0a363659757f786978e7f6f93d9
Ryan Navarro <ryannavarro@ymail.com>
2017-10-28 20:41:42 -0700

    Main Files

---
+++ scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  var $menuTab = $("#menutab");
+  var $menuList = $('.overlay');
+  var $menuItems = $("li");
+  var $signUp = $(".signupbtn")
+  var $cancel = $(".cancel")
+
+  //click on icon show and hide menu
+  $menuTab.click(function(){
+    $menuTab.toggleClass("rotate");//rotate icon on click
+    $menuList.toggleClass("showmenu"); //show menu
+    //blur background of main content
+  });
+  //click on list item hidemenu
+  $menuItems.on("click", function(){
+    $menuList.toggleClass("showmenu");//hide menu
+    $menuTab.toggleClass("rotate");// rotate icon back
+  });
+
+  //click on signup to show signup box
+  $signUp.on("click",function(){
+    $(".signup").show("slow");
+  })
+
+  //hide sign up
+  $cancel.click(function(){
+    $(".signup").hide("slow");
+  });
+
+  
+});

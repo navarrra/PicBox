c9d0d10327ccf68c7c18df4a7d676d24e41a58e4
Ryan Navarro <ryannavarro@ymail.com>
2017-11-06 10:30:44 -0800

    fixed api style front search page
    fixed api to search for citys through search box. styled search page.

--- scripts.js
+++ scripts.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
   var $menuTab = $("#menutab");
   var $menuList = $('.overlay');
   var $menuItems = $("li");
-  var $signUp = $(".signupbtn")
-  var $cancel = $(".cancel")
+  var $signUp = $(".signupbtn");
+  var $cancel = $(".cancel");
 
   //click on icon show and hide menu
   $menuTab.click(function(){
@@ -32,9 +32,9 @@ $(document).ready(function(){
   //ajax request to connect to api
   function getWeather(city){
   var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=";
-  var weatherCity = "http://api.openweathermap.org/data/2.5/weather?q="
+  var weatherCity = "http://api.openweathermap.org/data/2.5/weather?"
   var apiKey = "&APPID=8d3bcd3e412a8f4ca6bb2b0183ea2c78";
-  var city = "q=losangeles";
+  var city = "q=" + $(".searchbox").val();
   var metric = "units=metric";
   var imperial = "units=imperial";
 
@@ -61,16 +61,3 @@ $(document).ready(function(){
 
 
 });
-
-
-// var xhr = new XMLHttpRequest(),
-//     method = "GET",
-//     url = "https://api.flickr.com/services/rest/?method=flickr.test.echo&name=value";
-//
-// xhr.open(method, url, true);
-// xhr.onreadystatechange = function () {
-//   if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-//     console.log("ready");
-//   }
-// };
-// xhr.send();

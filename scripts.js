4b393118a6dfd64c5fb1edd750c75d22204508a1
Ryan Navarro <ryannavarro@ymail.com>
2017-11-06 09:13:22 -0800

    changed to Weather box app
    changed to weather box and back ground color to blue on top bar. make ajax request and respoinse in console log

--- scripts.js
+++ scripts.js
@@ -27,30 +27,37 @@ $(document).ready(function(){
     $(".signup").hide("slow");
   });
 
+  $(".searchBtn").click(getWeather);
+
   //ajax request to connect to api
+  function getWeather(city){
   var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=";
-  var apiKey = "8d3bcd3e412a8f4ca6bb2b0183ea2c78";
-  var city = "";
-  var options = {
-    tags: "panda",
-    format:"json"
-  };
+  var weatherCity = "http://api.openweathermap.org/data/2.5/weather?q="
+  var apiKey = "&APPID=8d3bcd3e412a8f4ca6bb2b0183ea2c78";
+  var city = "q=losangeles";
+  var metric = "units=metric";
+  var imperial = "units=imperial";
 
-  var jqxhr = $.ajax( weatherAPI + apiKey )
-    .done(function() {
-      alert( "success" );
-    })
-    .fail(function() {
-      alert( "error" );
-    })
-    .always(function() {
-      alert( "complete" );
-    });
 
-  $("searchBtn").click(function(event){
-    event.preventDefault();
-    $.getJSON(weatherAPI, options, displayPhotos);
+  $.ajax({
+    url: weatherCity + city + imperial + apiKey,
+    success: function(data){
+      console.log("success", data);
+    },
   });
+};
+  // var jqxhr = $.ajax( weatherAPI + apiKey )
+  //   .done(function() {
+  //     alert( "success" );
+  //   })
+  //   .fail(function() {
+  //     alert( "error" );
+  //   })
+  //   .always(function() {
+  //     alert( "complete" );
+  //   });
+
+
 
 
 });

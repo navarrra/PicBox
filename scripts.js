eb582a71f973c97ccc9f8a0f3555c5549f35d1b0
Ryan Navarro <ryannavarro@ymail.com>
2017-12-06 19:06:56 -0800

    Restructured app around api call from undergorund weather
    switched api to underground weather and changed all the connecting names. removed sencond loop to reduce redundencies and implemented CF button statement into the sucess function.

--- scripts.js
+++ scripts.js
@@ -53,77 +53,78 @@ $(document).ready(function(){
     });
   });
 
-
-
-  //ajax request to connect to api
-  function getWeather(city){
-  var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=";
-  var weatherCity = "http://api.openweathermap.org/data/2.5/weather?"
-  var apiKey = "&APPID=8d3bcd3e412a8f4ca6bb2b0183ea2c78";
-  var city = "q=" + $(".searchbox").val();
+  var apiKey = "72524e825328238b";
   var metric = "&units=metric";
   var imperial = "&units=imperial";
   var fcButton;
 
-  //if statement for metric and imperial unit assignment
-  if($("#fbutton").val()==="on"){
-     fcButton = "&units=imperial";
-  }else{
-    fcButton = "&units=metric";
-  }
+  //ajax request to connect to api
+  function getWeather(city){
+  // var urls = [/*"http://api.openweathermap.org/data/2.5/forecast?"*/, "http://api.openweathermap.org/data/2.5/weather?"];
+  var weatherCity = "http://api.wunderground.com/api/"
+  var city = "/conditions" + "/q/" + $(".searchbox").val() + ".json";
 
+
+  // //if statement for metric and imperial unit assignment
+  // if($("#fbutton").val()==="on"){
+  //    fcButton = data.current_observation.temp_f;
+  // }else{
+  //   fcButton = data.current_observation.temp_c;
+  // }
 
   $.ajax({
-    url: weatherCity + city + fcButton + apiKey,
+    url: weatherCity + apiKey + city,
     success: function(data){
-
+      var temp = document.getElementById("temp_f");
       var cityName = document.getElementById("city");
-      var description = ["description", "icon"];
-      var highlow = ["temp" ,"humidity"];
+      var description = ["weather", "icon_url", "precip_today_in"];
+      var highlow = [ ,"humidity"];
+
+      if($("#fbutton").val()==="on"){
+         fcButton = temp.innerHTML = Math.round(data.current_observation.temp_f) + '°';
+      }else{
+         fcButton = temp.innerHTML = Math.round(data.current_observation.temp_c) + '°';
+      }
 
         //background switch statement
-        var background = data.weather['0'].icon;
+        var background = data.current_observation.icon;
         switch(background){
-            case '01n':
-            case '01d': /*consolelog temp*/
+
+            case 'clear':
+            case 'sunny':
+            case 'mostlysunny': /*consolelog temp*/
               console.log("1");    $(".searchResults").css("background-image", "url('http://allswalls.com/images/clear-sky-wallpaper-3.jpg')");
                 break;
-            case '02n':
-            case '02d': /*consolelog temp*/
+            case 'partlycloudy':
+            case 'partlysunny': /*consolelog temp*/
               console.log("1");    $(".searchResults").css("background-image", "url('https://aujasus.files.wordpress.com/2011/04/amazon-and-the-cloud.jpg')");
                 break;
-            case '03n':
-            case '03d': /*consolelog temp*/
+            case 'scatteredclouds':  /*consolelog temp*/
               console.log("1");    $(".searchResults").css("background-image", "url('https://images.freeimages.com/images/large-previews/c1a/blue-sky-and-clouds-1400674.jpg')");
                 break;
-            case '04n':
-            case '04d': /*consolelog temp*/
+            case 'mostlycloudy':
+            case 'overcast': /*consolelog temp*/
               console.log("1");    $(".searchResults").css("background-image", "url('https://img00.deviantart.net/fa39/i/2015/052/6/1/broken_clouds_by_leo_6tos-d8ixdlv.jpg')");
                 break;
-            case '09n':
-            case '09d': /*consolelog temp*/
+            case 'sleet':
+            case 'rain':
               console.log("1");    $(".searchResults").css("background-image", "url('https://i.pinimg.com/736x/3c/88/a4/3c88a4c7142345ccf72b69afd51301a9--rainy-morning-iphone-wallpaper.jpg')");
                 break;
-            case '10n':
-            case '10d': /*consolelog temp*/
-              console.log("1");    $(".searchResults").css("background-image", "url('https://i.pinimg.com/736x/3c/88/a4/3c88a4c7142345ccf72b69afd51301a9--rainy-morning-iphone-wallpaper.jpg')");
-                break;
-            case '11n':
-            case '11d': /*consolelog temp*/
+            case 'tstorms':
               console.log("1");    $(".searchResults").css("background-image", "url('https://i.pinimg.com/736x/ca/3b/ff/ca3bff05bbaa2a87865de9a1668c6884--lightning-storms-lightning-strikes.jpg')");
                 break;
-            case '13n':
-            case '13d': /*consolelog temp*/
+            case 'snow':
+            case 'flurries': /*consolelog temp*/
               console.log("1");    $(".searchResults").css("background-image", "url('https://arch5541.files.wordpress.com/2012/11/snow.jpg')");
                 break;
-            case '50n':
-            case '50d': /*consolelog temp*/
+            case 'fog':
+            case 'hazy': /*consolelog temp*/
               console.log("1");    $(".searchResults").css("background-image", "url('https://i.imgur.com/dsQ2LMh.jpg')");
                 break; /*consolelog temp*/
             default:$(".searchResults").css("background-image", "url('https://i.imgur.com/dsQ2LMh.jpg')");
             console.log('2');
 
-        };//end
+        };//end of switch state
 
 
         console.log("success", data);
@@ -134,51 +135,60 @@ $(document).ready(function(){
         //show results after search
         $(".searchResults").removeClass("hideSearchresults");
 
-        // if($(window).width()<670){
-        //   $(".subnav").addClass("hideSearchresults");
-        // }else {
-        //   $("subnav").removeClass("hideSearchresults")
-        // }
 
         //data displayed from object results for city and current temp
-          cityName.innerHTML = data.name + ", " + data.sys.country;
+         cityName.innerHTML = data.current_observation.display_location.full;
 
           //loop through weather array
-        $.each(data.weather["0"], function(i){
+        $.each(data.current_observation, function(i){
             $.each(description, function(j){
                 if(description[j]==[i]){
-                    document.getElementById(description[j]).innerHTML = data.weather["0"][i];
-                    if(description[j]=="icon"){
-                      var icon = data.weather["0"].icon;
+                    document.getElementById(description[j]).innerHTML = data.current_observation[i];
+                    if(description[j]=="icon_url"){
+                      var icon = data.current_observation.icon_url;
                       //append icon
-                      console.log(data.weather["0"].icon)
-                      $("#icon").prepend("<img src='http://openweathermap.org/img/w/" + icon + ".png'>" );
+                      console.log(data.current_observation.icon_url);
+                      $("#icon_url").prepend("<img src='" + icon + "'>" );
                     };
                 };
             });
-          });//end
+          });//end of loop
 
 
-            //loop through array
-          $.each(highlow, function(i){
-              //loop through api main
-              $.each(data.main, function(j){
-                //if statements for matching class with object
-                if(highlow[i]==[j]){
-                  //if statement for degree symbol
-                    if(highlow[i]=="temp"){
-                    document.getElementById(highlow[i]).innerHTML = Math.round(data.main[j]) + "°";
-                    //else statement for percentage symbol
-                  }else{
-                    document.getElementById(highlow[i]).innerHTML = Math.round(data.main[j]) + "%";
-                  };
-                };
+          //   //loop through array
+          // $.each(highlow, function(i){
+          //     //loop through api main
+          //     $.each(data.main, function(j){
+          //       //if statements for matching class with object
+          //       if(highlow[i]==[j]){
+          //         //if statement for degree symbol
+          //           if(highlow[i]=="temp"){
+          //           document.getElementById(highlow[i]).innerHTML = Math.round(data.main[j]) + "°";
+          //           //else statement for percentage symbol
+          //         }else{
+          //           document.getElementById(highlow[i]).innerHTML = Math.round(data.main[j]) + "%";
+          //         };
+          //       };
+          //     });
+          //   });//end of loop
+            //started 5 day forcast api call
+              $(".fiveday").click(function(){
+                var url = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+                $.ajax({
+                  url: url + city + apiKey,
+                  success: function(data){
+                    console.log(data);
+                    console.log("winner");
+                },
               });
-            });//end
+            });
 
-    },
+
+
+      }, //end of success
+
+    });//end of ajax
+
+}; //end of getWeather function
 
   });
-};
-
-});

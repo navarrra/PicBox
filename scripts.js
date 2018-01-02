cbeb1438f7ccff285d92034d2c5db390abf758e2
Ryan Navarro <ryannavarro@ymail.com>
2018-01-02 14:29:34 -0800

    fixed the celsus bitton for 3day and fixed the error message

--- scripts.js
+++ scripts.js
@@ -7,71 +7,79 @@ $(document).ready(function(){
   var $signUp = $(".signupbtn");
   var $cancel = $(".cancel");
 
-  //click on icon show and hide menu
-  $menuTab.click(function(){
-    $menuTab.toggleClass("rotate");//rotate icon on click
-    $menuList.toggleClass("showmenu"); //show menu
-    //blur background of main content
-  });
 
-  //click on list item hidemenu
-  $menuItems.on("click", function(){
-    $menuList.toggleClass("showmenu");//hide menu
-    $menuTab.toggleClass("rotate");// rotate icon back
-  });
-
-  //click on signup to show signup box
-  $signUp.on("click",function(){
-    $(".signup").show("slow");
-  })
-
-
-  //hide sign up
-  $cancel.click(function(){
-    $(".signup").hide("slow");
-  });
-
-  //Search button with ajax call function
-  $(".searchBtn").click(getWeather);
-
-//back button
-  $(".backbtn, .home, .searchsub").click(function(){
-    $(".searchResults").addClass("hideSearchresults");
-    $(".welcomepage").show();
-    $(".searchbox").val("");
-  });
-
-  //CF buttons
-  $("#cbutton").click(function(){
-    //turn on Cbutton turn off fbutton
-    $(this).addClass("onbtn").val("on");
-      $("#fbutton").removeClass("onbtn").val("off");
-      //turn on fbutton turn off cbutton
-      $("#fbutton").click(function(){
-        $(this).addClass("onbtn").val("on");
-          $("#cbutton").removeClass("onbtn").val("off");
-    });
-  });
-
-  //started underline function for large submenu
-  $(".three_day").click(function(){
-        $(".current").removeClass("underline");
-        $(this).addClass("underline");
-        $(".current").click(function(){
-            $(this).addClass("underline");
-            $(".three_day").removeClass("underline");
-        });
-  });
-
-  //click on current submenu to move it back to center
-  $(".current").click(function(){
-        $("#current").show().animate({"left": "0%"});
-        $("#current").val("on");
-        $("#weekday_short").hide();
-        $(".three_day").click(function(){
-            $("#weekday_short").show();
-        });
+      //click on icon show and hide menu
+      $menuTab.click(function(){
+        $menuTab.toggleClass("rotate");//rotate icon on click
+        $menuList.toggleClass("showmenu"); //show menu
+        //blur background of main content
       });
+
+
+      //click on list item hidemenu
+      $menuItems.on("click", function(){
+        $menuList.toggleClass("showmenu");//hide menu
+        $menuTab.toggleClass("rotate");// rotate icon back
+      });
+
+
+      //click on signup to show signup box
+      $signUp.on("click",function(){
+        $(".signup").show("slow");
+      })
+
+
+      //hide sign up
+      $cancel.click(function(){
+        $(".signup").hide("slow");
+      });
+
+
+      //Search button with ajax call function
+      $(".searchBtn").click(getWeather);
+
+
+      //back button
+        $(".backbtn, .home, .searchsub").click(function(){
+          $(".searchResults").addClass("hideSearchresults");
+          $(".welcomepage").show();
+          $(".searchbox").val("");
+        });
+
+
+      //CF buttons
+      $("#cbutton").click(function(){
+        //turn on Cbutton turn off fbutton
+        $(this).addClass("onbtn").val("on");
+          $("#fbutton").removeClass("onbtn").val("off");
+          //turn on fbutton turn off cbutton
+          $("#fbutton").click(function(){
+            $(this).addClass("onbtn").val("on");
+              $("#cbutton").removeClass("onbtn").val("off");
+          });
+        });
+
+
+        //started underline function for large submenu
+        $(".three_day").click(function(){
+              $(".current").removeClass("underline");
+              $(this).addClass("underline");
+              $(".current").click(function(){
+                  $(this).addClass("underline");
+                  $(".three_day").removeClass("underline");
+              });
+            });
+
+
+      //click on current submenu to move it back to center
+      $(".current").click(function(){
+            $("#current").show().animate({"left": "0%"});
+            $("#current").val("on");
+            $("#weekday_short").hide();
+            $(".three_day").click(function(){
+                $("#weekday_short").show();
+            });
+          });
 
 
 
@@ -89,22 +97,23 @@ $(document).ready(function(){
   $.ajax({
     url: weatherCity + apiKey + conditions + city,
     success: function(data){
+      var type = true;
       var temp = document.getElementById("temp_f");
       var cityName = document.getElementById("city");
       var description = ["weather", "icon_url", "precip_today_in"];
       console.log(data)
         //if statement for metric and imperial unit assignment
-  try{
-      if($("#fbutton").val()==="on"){
-         fcButton = temp.innerHTML = Math.round(data.current_observation.temp_f) + '°';
-      }else{
-         fcButton = temp.innerHTML = Math.round(data.current_observation.temp_c) + '°';
-      };
+            try{
+                if($("#fbutton").val()==="on"){
+                   fcButton = temp.innerHTML = Math.round(data.current_observation.temp_f) + '°';
+                }else{
+                   fcButton = temp.innerHTML = Math.round(data.current_observation.temp_c) + '°';
+                   type="false";
+                };
+              } catch(error){
+                    $(".err").fadeIn().delay(1500).fadeOut();
+              };
 
-    } catch(error){
-          $(".error").append('<p class="err">please include city and state</p>');
-
-    };
 
         //background switch statement
         var background = data.current_observation.icon;
@@ -113,36 +122,44 @@ $(document).ready(function(){
             case 'clear':
             case 'sunny':
             case 'mostlysunny':
-              console.log("1");    $(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/sunnyback.jpg')");
+                  $(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/sunnyback.jpg')");
                 break;
+
             case 'partlycloudy':
             case 'partlysunny':
-              console.log("1");    $(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/partlycloudyback.jpg')");
+                  $(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/partlycloudyback.jpg')");
                 break;
+
             case 'scatteredclouds':
-              console.log("1");    $(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/scatteredback.jpg')");
+                  $(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/scatteredback.jpg')");
                 break;
+
             case 'mostlycloudy':
             case 'cloudy':
-              console.log("1");    $(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/cloudyback.jpg')");
+                  $(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/cloudyback.jpg')");
                 break;
+
             case 'sleet':
             case 'rain':
-              console.log("1");    $(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/rainback.jpg')");
+                  $(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/rainback.jpg')");
                 break;
+
             case 'tstorms':
-              console.log("1");    $(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/tstormsback.jpg')");
+                  $(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/tstormsback.jpg')");
                 break;
+
             case 'snow':
             case 'flurries':
-              console.log("1");    $(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/snowback.jpg')");
+                  $(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/snowback.jpg')");
                 break;
+
             case 'fog':
             case 'hazy':
-              console.log("1");    $(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/fogback.jpg')");
+                  $(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/fogback.jpg')");
                 break;
+
             default:$(".searchResults").css("background-image", "url('https://rawgit.com/navarrra/weatherBox/master/backgrounds/fogback.jpg')");
-            console.log('2');
+
 
         };//end of switch state
 
@@ -185,7 +202,6 @@ $(document).ready(function(){
                   success: function(data){
                     console.log(data);
                     var forecastDate= data.forecast.simpleforecast.forecastday;
-
                     var forecast = [forecastDate];
 
 
@@ -196,11 +212,16 @@ $(document).ready(function(){
                          for(var j = 1 ; j < threeDay.length; j++){
                          var days = threeDay[j].date.weekday_short;
                          var icon = threeDay[j].icon_url;
-                         var high = "<h3> High: " +  threeDay[j].high.fahrenheit + "°" + "</h3><br>";
-                         var low = "<h3> low: " +  threeDay[j].low.fahrenheit + "°" + "</h3>";
+                         var highf = "<h3> High: " +  threeDay[j].high.fahrenheit + "°" + "</h3><br>";
+                         var lowf = "<h3> low: " +  threeDay[j].low.fahrenheit + "°" + "</h3>";
+                         var highc = "<h3> High: " +  threeDay[j].high.celsius + "°" + "</h3><br>";
+                         var lowc ="<h3> low: " +  threeDay[j].low.celsius + "°" + "</h3>";
 
-                         $("#weekday_short").append("<div class='result3Day'><h3>"+ days + "</h3><br><img class='icon_url3' src='"+ icon +"'>" + high + low + "</div>");
-
+                         if(type==true){
+                           $("#weekday_short").append("<div class='result3Day'><h3>"+ days + "</h3><br><img class='icon_url3' src='"+ icon +"'>" + highf + lowf + "</div>");
+                          }else{
+                            $("#weekday_short").append("<div class='result3Day'><h3>"+ days + "</h3><br><img class='icon_url3' src='"+ icon +"'>" + highc + lowc + "</div>");
+                          };//end of else
                                 };
                               };
                           },
